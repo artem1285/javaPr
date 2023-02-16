8d3c79139d75a702e0acf6d2211cced54a3a13a6
Artem Alekseevgit <artem_1285@mail.ru>
2023-02-16 21:35:17 +0500

    2  спеминар java продолжение

--- sem_2.java
+++ sem_2.java
@@ -41,19 +41,48 @@ public class sem_2 {
 // далее будет сравнение этих строк и говорить, есть ли в первой строке 
 // элементы другой строки.
 
-Scanner scanner = new Scanner (System.in);
-    System.out.print("Ведите строку: ");
-     //перевели, что бы был не класс строка, а класс bilder
-     StringBuilder strOne = new StringBuilder(scanner.nextLine()); //одной строчкой инициализировали и засунули в StringBuilder строчку, которую ввел пользоватль
-     System.out.println(strOne);
-    System.out.println("Ведите вторую строку: ");
-    StringBuilder strTwo = new StringBuilder(scanner.nextLine()); //одной строчкой инициализировали и засунули в StringBuilder строчку, которую ввел пользоватль
-     System.out.println(strTwo);
-// что бы проверить есть ли одинаковые элементы в строках
-    // порверяем по условию
-    if(strOne.toString().contains(strTwo.toString())) {
-      System.out.println("содержит");
+// Scanner scanner = new Scanner (System.in);
+//     System.out.print("Ведите строку: ");
+//      //перевели, что бы был не класс строка, а класс bilder
+//      StringBuilder strOne = new StringBuilder(scanner.nextLine()); //одной строчкой инициализировали и засунули в StringBuilder строчку, которую ввел пользоватль
+//      System.out.println(strOne);
+//     System.out.println("Ведите вторую строку: ");
+//     StringBuilder strTwo = new StringBuilder(scanner.nextLine()); //одной строчкой инициализировали и засунули в StringBuilder строчку, которую ввел пользоватль
+//      System.out.println(strTwo);
+// // что бы проверить есть ли одинаковые элементы в строках
+//     // порверяем по условию
+//     if(strOne.toString().contains(strTwo.toString())) {
+      
+// // Задача 2.
+//     // Находим в одной строке другую и если находим ее, 
+//     // то меняем надписью “найденая строка заменена на другую”, 
+//    // если не находим, “совпадений ненайдено”. 
+      
+//    //если strOne содержит strTwo, то получается в strOne приминяем метод replase
+//    //и меняем strTwo на фразу 
+//    // и потом всю эту строчку обернуть в System.out.println 
+//     //     System.out.println(strOne.toString().replace(strTwo.toString() ,"найденая строка заменена на другую"));
+//     // }
+//     //     else  System.out.println("совпадений ненайдено");
 
-    }
-  } 
+// // Задача 3.
+// // Если сравнивали contains(strTwo.toString, теперь надо сравнить contains bilder
+//         int startindex = strOne.indexOf(strTwo.toString()); 
+//         System.out.println(startindex);
+//         int endindex = startindex + strTwo.length();
+//         System.out.println(endindex);
+//         System.out.println(strOne.replace(startindex, endindex, "Что надо было написать ?"));
+//         }
+//             else  System.out.println("совпадений ненайдено");
+ // Задача 4.
+// Сравнить время замены 3-5 значений в строке методом string replase и stringBilder replase 
+   
+StringBuilder strOne = new StringBuilder("Hello world");
+String strTwo = "Hello world";
+long begin = System.currentTimeMillis();
+System.out.println(strOne.replace(0, 0, strTwo)); // заменить например какие то буквы, это делали на семинаре
+long end = System.currentTimeMillis();
+System.out.println(end-begin);
+
+      }
 }
