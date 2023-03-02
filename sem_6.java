39fe4a279ee082083c341021fec3710736956f90
Artem Alekseevgit <artem_1285@mail.ru>
2023-03-02 21:32:04 +0500

    1:45:43

--- sem_6.java
+++ sem_6.java
@@ -18,6 +18,10 @@ set.add(10);
 System.out.println(set);
 System.out.println(set.contains(11));
 System.out.println(set.contains(5));
+System.out.println(set.get(5));
+System.out.println(set.getArrayList());
+System.out.println(set.headSet(7));
+System.out.println(set.tailSet(7));
 
 }
 }
@@ -25,6 +29,7 @@ System.out.println(set.contains(5));
 class MySet{
     private HashMap <Integer, Object> values = new HashMap <> ();
    private static final Object X = new Object();
+private static final String ArrayList = null;
     
     void add (int data){
    values.put(data, X);
@@ -54,6 +59,58 @@ class MySet{
 // метод, который будет возвращает элемент по индексу
    int get(int ind){
     return (int) values.keySet().toArray()[ind];
-  
    }
-  }
+    /*Сделать метод, который будет возвращать Arraylist . 
+    Т.е. он будет создавать Arraylist в котором будут наши ключи 
+    и возвращать Arraylist  */
+ArrayList getArrayList(){
+    ArrayList <Integer> res = new ArrayList<>(); 
+    values.forEach((k,v) -> res.add(k));
+    return res;
+
+}
+    /*Сделать 2 метода, TeilSet, принимает в параметре ключ, 
+а возвращает наш Set, но клбчи в нем указанные нами и до конца Set.
+И HeadSet который тоже принимает ключ ,но он будет сначала Set 
+и до указанного нами. */
+
+MySet headSet (int data){ 
+       MySet res = new MySet();
+    for (int el : values.keySet()) {
+        res.add(el);
+        if(data ==el) 
+        break;
+    }
+    return res;
+}
+MySet tailSet (int data){
+    boolean ad = false;
+    MySet res = new MySet();
+    for (int el : values.keySet()) {
+        if(data ==el) ad = true;
+        if(ad){res.add(el);}
+       
+    }
+    return res;
+}
+    /*SubSet. Вы даем два ключа и он возвращает MySet начиная с  */
+    MySet subSet (int key1, int key2){
+        MySet res = new MySet();
+        boolean ad = false;
+        for (int el : values.keySet()){
+            if(el == key1 || el == key2){
+                ad = true;
+            }
+            if(ad){
+                res.add(el);
+                if(el == key1 || el == key2){
+                   break;
+                }
+
+            }
+        }
+        return res;   
+    }
+}
+
+
