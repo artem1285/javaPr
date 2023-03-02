990e4a04b0f1997f153a9feba6da1b7b814d1e34
Artem <artem_1285@mail.ru>
2023-03-02 15:55:10 +0500

    1:14:50

--- sem_6.java
+++ sem_6.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class sem_6 {
@@ -10,18 +11,49 @@ public class sem_6 {
 MySet set = new MySet();
 set.add(5);
 set.add(6);
+set.add(7);
+set.add(8);
+set.add(9);
+set.add(10);
+System.out.println(set);
+System.out.println(set.contains(11));
+System.out.println(set.contains(5));
+
 }
 }
 
 class MySet{
-    HashMap <Integer, Object> values = new HashMap <> ();
+    private HashMap <Integer, Object> values = new HashMap <> ();
    private static final Object X = new Object();
     
     void add (int data){
    values.put(data, X);
     }
     // метод вывода
-    public String toString(){
-        values.forEach((k,v) -> System.out.println (k ));   
-    }
-}
+    public String toString() {
+       return values.keySet().toString();
+    } 
+       // получить доступ (считать) к элементам нашего списка
+   Boolean contains (int data){
+    return values.containsKey(data);
+   }
+   //очистка
+   void clear () {
+    values.clear();
+   }
+   // удаление элемента
+   void remove(int data){
+    values.remove(data);
+   }
+   /*Сделать метод, который будет возвращать количество 
+   элементов в сете, и метод, который будет возвращать 
+   элемент по индексу.*/
+   int size(){
+    return values.size(); 
+   } //возвращает количество элементов
+// метод, который будет возвращает элемент по индексу
+   int get(int ind){
+    return (int) values.keySet().toArray()[ind];
+  
+   }
+  }
